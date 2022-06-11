0b8b92a718cf91007e025ba529ba4dcdbcf3183a
Astaroth- <88864348+Astaroth-99@users.noreply.github.com>
2022-06-11 20:55:25 -0300

    Calculadora

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+
+ReactDOM.render(
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator />
+    <h2>Juliana santos</h2>
+  </div>,
+  document.getElementById('root')
+);
